31a0fc14a5168c841bc0bb8938d80d2286c5db7d
Angel Diaz <surferdie141@fullsail.edu>
2013-02-09 13:40:57 -0500

    adding code igniter to repo

---
+++ Project/NewLeaf/application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>New Leaf - Welcome</title>
+		<link rel="stylesheet" href="../css/themes/newLeaf.min.css" />
+		<link rel="stylesheet" href="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile.structure-1.3.0-rc.1.min.css" />
+</head>
+<body>
+
+<div id="container">
+	<h1>HAHAHAHAHAHA</h1>
+
+	<div id="body">
+<script src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script src="http://code.jquery.com/mobile/1.3.0-rc.1/jquery.mobile-1.3.0-rc.1.min.js"></script>
+	</div>
+</div>
+
+</body>
+</html>
